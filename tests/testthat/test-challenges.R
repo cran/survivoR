312e83e283c20128acc544cdc892a9539cdbb828
Daniel Oehm <danieloehm@gmail.com>
2022-05-31 12:20:02 +0000

    version 1.0.1

--- tests/testthat/test-challenges.R
+++ tests/testthat/test-challenges.R
@@ -1,4 +1,10 @@
 
+library(dplyr)
+library(purrr)
+library(stringr)
+
+context("Challenges")
+
 test_that("No challenge without at least 1 descriptive feature", {
 
   x <- challenge_description |>
@@ -19,5 +25,25 @@ test_that("No challenge without at least 1 descriptive feature", {
     nrow()
 
   expect_equal(x, 11)
+
+})
+
+
+test_that("No NAs in castaway_ids", {
+
+  x <- survivoR::challenge_results |>
+    filter(challenge_name != "Survivor Auction") |>
+    unnest(winners)
+
+  expect_equal(all(!is.na(x$winner_id)), TRUE)
+
+})
+
+test_that("No NAs in challenge_ids (Challenge Results)", {
+
+  x <- survivoR::challenge_results |>
+    filter(challenge_name != "Survivor Auction")
+
+  expect_equal(all(!is.na(x$challenge_id)), TRUE)
 
 })
